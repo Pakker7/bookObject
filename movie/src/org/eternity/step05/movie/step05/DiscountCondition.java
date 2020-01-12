32d707234205240cc9083652db655d4e1f85b687
Pakker7 <43773175+Pakker7@users.noreply.github.com>
2020-01-12 22:37:37 +0900

    no message

--- movie/src/org/eternity/step05/movie/step05/DiscountCondition.java
+++ movie/src/org/eternity/step05/movie/step05/DiscountCondition.java
@@ -17,10 +17,10 @@ public class DiscountCondition {
         this.sequence = sequence;
     }
 
-    public DiscountCondition(DayOfWeek dayOfWeek, LocalTime startTime, LocalTime endTime){
-        this.type = DiscountConditionType.PERIOD;
-        this.dayOfWeek= dayOfWeek;
-        this.startTime = startTime;
+    public DiscountCondition(DayOfWeek dayOfWeek, LocalTime startTime, LocalTime endTime){ // <--여기로 초기화 될때 15줄의 sequence는 초기화가 안됨
+        this.type = DiscountConditionType.PERIOD;										// 따라서 초기화 시에 초기화 되는게 있고 안되는게 있다는 말임
+        this.dayOfWeek= dayOfWeek;														// 따라서 이게 응집도가 낮은 클래스라는거임
+        this.startTime = startTime;														// 응집도가 높은 클래스는 초기화 시에 한번에 다 된다고 함
         this.endTime = endTime;
     }
 
